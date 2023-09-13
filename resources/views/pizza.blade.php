bc684b6ad2ff8831ef90a7b863beb9026ef56604
EvaristusTech <arinzeevaristus640@gmail.com>
2023-09-13 16:32:17 +0100

    Learning loops in blade syntax

--- resources/views/pizza.blade.php
+++ resources/views/pizza.blade.php
@@ -12,3 +12,11 @@
     $name = 'my name is eva tech a self taught programmer :)';
     echo "$name";
 @endphp
+
+<br><br><br><br>
+
+<h1>loops in laravel/blade....</h1>
+
+@for($i = 0; $i <= 10; $i++)
+    <p>this is loop {{ $i }}</p>
+@endfor
